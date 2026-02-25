20d247b970a185e1f628c1c4a0764acccf039511
Deepa131 <deepapaudel65@gmail.com>
2026-02-26 02:27:07 +0545

    updated layout of owner side

--- app/(dashboard)/owner/layout.tsx
+++ app/(dashboard)/owner/layout.tsx
@@ -27,7 +27,7 @@ export default async function OwnerLayout({
   }
 
   return (
-    <div className="min-h-screen w-full bg-gradient-to-br from-gray-50 to-gray-100">
+    <div className="min-h-screen w-full bg-linear-to-b from-blue-50 to-white">
       <DashboardHeader
         userName={userData.fullName}
         userRole={userData.role}
