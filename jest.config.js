11b7f9a9056003614ca0ffbaf37b33337e70b878
Deepa131 <deepapaudel65@gmail.com>
2026-02-26 03:25:23 +0545

    added dependencies

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+}
+
+module.exports = createJestConfig(config)
